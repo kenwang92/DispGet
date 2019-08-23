bdf29311e8beeafbec855b408003876a50bd5097
kenwang92 <51316165+kenwang92@users.noreply.github.com>
2019-08-23 19:15:21 +0800

    Update 爬蟲.py

--- 爬蟲.py
+++ 爬蟲.py
@@ -6,7 +6,7 @@ import re
 print('PPT開爬')
 
 UPurl = requests.get('https://disp.cc/b/Gossiping')
-soup = BeautifulSoup(UPurl.text)
+soup = BeautifulSoup(UPurl.text,'html.parser')
 UP = soup.find_all('a',{"title":"(PgUp,P)"})
 UP = str(UP)
 UPlist = re.findall(r'\d+',UP)
@@ -14,13 +14,13 @@ UPlist = re.findall(r'\d+',UP)
 while(1):
     url = 'https://disp.cc/b/Gossiping?pn='+UPlist[0]+'&init=0'
     r = requests.get(url)
-    soup = BeautifulSoup(r.text)
+    soup = BeautifulSoup(r.text,'html.parser')
     for soup in soup.find_all('span',{"class": "titleColor"}):
         soup = str(soup)
         soup = soup.strip('<span class="titleColor">■</span>')
         print(soup)
     UPurl = requests.get(url)
-    soup = BeautifulSoup(UPurl.text)
+    soup = BeautifulSoup(UPurl.text,'html.parser')
     UP = soup.find_all('a',{"title":"(PgUp,P)"})
     UP = str(UP)
     UPlist = re.findall(r'\d+',UP)
